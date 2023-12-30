53713f16c9992054acfe5900456a17674f2e098a
m_masashi <drift_tms@yahoo.co.jp>
2023-12-30 12:34:13 +0900

    BowlingGameの空テンプレートを作成

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
-#include "fizzbuzz.h"
+#include "bowling.h"
 
 int main(void)
 {
-    const int RepeatCount = 100;
-    for (int i = 0; i < RepeatCount; i++)
-    {
-        int number = i + 1;
-        char buffer[100]; // 十分とっておく
-        ToFizzBuzzNumber(number, buffer, sizeof(buffer));
-        printf("%s\n", buffer);
-    }
+    printf("Hello, World!\n");
     return 0;
 }
