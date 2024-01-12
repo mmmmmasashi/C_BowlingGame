4a507821d0ea8ec9daeb560e398a7758fb806e7a
m_masashi <drift_tms@yahoo.co.jp>
2024-01-12 21:54:49 +0900

    リファクタ : テストコードを整理。setupとRollManyTimes導入

--- test/GameTest.cpp
+++ test/GameTest.cpp
@@ -1,31 +1,50 @@
 #include "CppUTest/CommandLineTestRunner.h"
 #include "Game.h"
 
-TEST_GROUP(GameTestGroup){};
+TEST_GROUP(GameTestGroup){
+    Game* game;
+    
+    void setup(void)
+    {
+        game = Game_Create();
+    }
+
+    void RollManyTimes(int pinNum, int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            Game_Roll(game, pinNum);
+        }
+    }
+};
 
 TEST(GameTestGroup, AllGutter)
 {
-    Game* game = Game_Create();
-
-    for (int i = 0; i < 20; i++)
-    {
-        Game_Roll(game, 0);
-    }
-    
+    RollManyTimes(0, 20);
     CHECK_EQUAL(0, Game_Score(game));
 }
 
 TEST(GameTestGroup, NoMark)
+{
+    Game_Roll(game, 1);
+    Game_Roll(game, 2);
+
+    RollManyTimes(0, 2 * 9);
+    
+    CHECK_EQUAL(3, Game_Score(game));
+}
+
+TEST(GameTestGroup, NoMark_2Frames)
 {
     Game* game = Game_Create();
 
     Game_Roll(game, 1);
     Game_Roll(game, 2);
 
-    for (int i = 0; i < 2 * 9; i++)
-    {
-        Game_Roll(game, 0);
-    }
+    Game_Roll(game, 3);
+    Game_Roll(game, 4);
+
+    RollManyTimes(0, 2 * 8);
     
-    CHECK_EQUAL(3, Game_Score(game));
+    CHECK_EQUAL(3 + 7, Game_Score(game));
 }
